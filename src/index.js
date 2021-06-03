3b876549e285f25b9f8a32be5f3f976e874fcdb3
Gabriel Pizarro <gpizarro@javaman.net>
2021-06-03 15:18:35 -0700

    update router

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route exact path='/' component={Home} />
       </Switch>
