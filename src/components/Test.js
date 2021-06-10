29c7677a1d58b6fc11574c355a200a9181ab8c4b
Gabriel Pizarro <gpizarro@javaman.net>
2021-06-10 13:23:45 -0700

    Bottom button

--- src/components/Test.js
+++ src/components/Test.js
@@ -4,36 +4,45 @@ import '../style/Test.css';
 export default class Test extends React.Component {
   constructor(props) {
     super(props);
-    this.setState({
+    this.state = {
       score: null
-    });
+    };
+  }
+
+  score() {
+    alert('hi');
   }
 
   render() {
     return (
       <div className='test'>
-        { this.score
+        { this.state.score
           ? <div className='test-result'></div>
-          : <div className='test-questions'>{
-            Object.entries(this.props.questions).map((data1) => {
-              let index = data1[0];
-              let question = data1[1];
-              return <div key={'question-' + index} className='test-question'>
-                <div className='test-question-index'><p>{index + '. '}</p></div>
-                <p className='test-question-title'>{question.title}</p>
-                <div className='test-question-options'>{
-                  Object.entries(question.levels).map((data2) => {
-                    let level = data2[0];
-                    let statement = data2[1];
-                    return <div key={'question-' + index + '-option-' + level} className='test-question-option'>
-                      <input className='test-question-radio' type='radio' id={index + '-' + level} name={index}></input>
-                      <label className='test-question-statement' htmlFor={index + '-' + level}>{statement}</label>
-                    </div>
-                  })
-                }</div>
-              </div>;
-            })
-          }</div>
+          : <div className='test-questions'>
+            {
+              Object.entries(this.props.questions).map((data1) => {
+                let index = data1[0];
+                let question = data1[1];
+                return <div key={'question-' + index} className='test-question'>
+                  <div className='test-question-index'><p>{index + '. '}</p></div>
+                  <p className='test-question-title'>{question.title}</p>
+                  <div className='test-question-options'>{
+                    Object.entries(question.levels).map((data2) => {
+                      let level = data2[0];
+                      let statement = data2[1];
+                      return <div key={'question-' + index + '-option-' + level} className='test-question-option'>
+                        <input className='test-question-radio' type='radio' id={index + '-' + level} name={index}></input>
+                        <label className='test-question-statement' htmlFor={index + '-' + level}>{statement}</label>
+                      </div>
+                    })
+                  }</div>
+                </div>;
+              })
+            }
+            <div className='test-button-container'>
+              <button className='test-button' onClick={this.score}><p>Submit</p></button>
+            </div>
+          </div>
         }
       </div>
     );
